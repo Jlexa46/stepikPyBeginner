329c5f0ac8f331401f9a93f5d72a11debfba3b59
Jlexa46 <lexa140191@gmail.com>
2022-03-20 11:40:30 +0300

    Complete Lesson_13.1

---
+++ Lesson_13/Lesson_13.1/Task_13.1b.py
@@ -0,0 +1,3 @@
+def draw_triangle():
+    print(*['*' * i for i in range(1, 11)], sep='\n')
+draw_triangle()
---
+++ Lesson_13/Lesson_13.1/Task_13.a.py
@@ -0,0 +1,5 @@
+def draw_box():
+    print('*' * 10)
+    print(('*' + ' ' * 8 + '*' + '\n') * 12, end='')
+    print('*' * 10)
+draw_box()
